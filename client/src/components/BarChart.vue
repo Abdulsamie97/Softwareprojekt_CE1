2d6bb063bbb0de12082280a9e42f8b6f4948a09c
Asma Jeljeli <Asma.Jeljeli@Student.HTW-Berlin.de>
2022-07-13 15:49:43 +0200

    Korrektur

--- client/src/components/BarChart.vue
+++ client/src/components/BarChart.vue
@@ -1,112 +1,66 @@
 <template>
-<panel title="BarChart">
+  <panel title="Songs">
 
-  <v-simple-table dark fixed-header
-    height="300px" width="100%">
-      <thead>
-        <tr>
-          <th class="text-left">
-            lyricslength
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="song in users" :key="song.id"
-        >
-          <td>{{song.lyricslength}}</td>
-          <td> </td>
-          <td>{{song.name}}</td>
-        </tr>
-      </tbody>
-  </v-simple-table>
- </panel>
-<!--<article>
-  <section class="ml-4 text-2x text-blue-500" v-for="(User, email) in User" v-bind:key="id">
-    {{User.email}}
+      <div v-for="song in songs" :key="song.id">
+       <v-layout>
+        <v-flex xs6>
+          <div class="song-title">
+            {{song.title}}
+          </div>
 
-  </section>  </article> -->
+        </v-flex>
 
-  <!-- <Bar
-    :chart-options="chartOptions"
-    :chart-data="chartData"
-    :chart-id="chartId"
-    :dataset-id-key="datasetIdKey"
-    :plugins="plugins"
-    :css-classes="cssClasses"
-    :styles="styles"
-    :width="width"
-    :height="height"
-  />-->
+        <v-flex xs6>
+          <div class="lyricsLength">
+          {{song.lyrics}}
 
+          </div>
+        </v-flex>
+       </v-layout>
+      </div>
+    </Panel>
 </template>
 
 <script>
-import BarChart from '@/services/SongsService'
+import SongsService from '@/services/SongsService'
+
 export default {
   data () {
     return {
-      users: null
+      songs: null
     }
   },
   watch: {
     '$route.query.search': {
       immediate: true,
       async handler (value) {
-        this.users = (await BarChart.index(value)).data
+        this.songs = (await SongsService.index(value)).data
+        const arr = this.songs.lyrics.split(' ')
+        this.songs.lyrics = arr.filter(word => word !== ' ').length
+        console.log(this.arr[1])
       }
     }
   }
 }
 
-/* import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart'
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
-} */
 </script>
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 30px;
+}
+.song-artist {
+  font-size: 24px;
+}
+.song-genre {
+  font-size: 18px;
+}
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
