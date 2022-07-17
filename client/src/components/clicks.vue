a7890c9eb388666918ba046f7c7fccd51a54fbf8
Nechirvan Haso Sodo Domili <nechirvan.domili@student.htw-berlin.de>
2022-07-17 21:25:32 +0000

    neue Seite für Anzeige der Clicksverhalten

---
+++ client/src/components/clicks.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <panel title="Clicks">
+
+  <v-simple-table dark fixed-header
+    height="300px" width="100%">
+      <thead >
+        <tr>
+          <th class="text-right">
+            Datum "  "
+          </th>
+          <th></th>
+          <th class="text-left">
+          Anzahl der Clicks
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="user in users" :key="user.id"
+        >
+          <td>{{user.email}}</td>
+          <td> </td>
+          <td>{{user.clearpassword}}</td>
+        </tr>
+      </tbody>
+  </v-simple-table>
+ </panel>
+<!--<article>
+  <section class="ml-4 text-2x text-blue-500" v-for="(User, email) in User" v-bind:key="id">
+    {{User.email}} -->
+</div>
+</template>
+<script>
+
+export default({
+
+})
+</script>
