83241ca744c7511430e160d894b14af9005be044
Mohamad Raafat Baki <mohamad.baki@student.htw-berlin.de>
2022-07-17 16:47:19 +0000

    Add new file(albums.vue), um API online hinzufügen.

---
+++ client/src/components/albums.vue
@@ -0,0 +1,15 @@
+<script>
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://api.discogs.com/artists/1/releases?page=2&per_page=75',
+ 
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+</script> 
